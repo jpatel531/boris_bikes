0e5e150c1beded101fe22fb7f2e6b10a7636d68e
Jamie Patel <jamiepatel@Jamies-MacBook-Pro.local>
2014-05-27 17:38:35 +0100

    Added capacity and capacity error

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,7 @@
 class DockingStation
-
-	def initialize
+	DEFAULT_CAPACITY = 15
+	def initialize(capacity_hash = {})
+		@capacity = capacity_hash.fetch(:capacity, DEFAULT_CAPACITY)
 		@bikes = []
 	end
 
@@ -9,6 +10,16 @@ class DockingStation
 	end
 
 	def dock(bike)
+		raise "Station is full" if full?
 		@bikes << bike
 	end
+
+	def release(bike)
+		@bikes.delete(bike)
+	end
+
+	def full?
+		bike_count == @capacity
+	end
+
 end
