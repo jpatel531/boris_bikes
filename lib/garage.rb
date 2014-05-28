4d537886c669c99d24c7caa68ff79093a89bfff8
Jamie Patel <jamiepatel@Jamies-MacBook-Pro.local>
2014-05-28 11:43:57 +0100

    Garage accepts multiple docked bikes

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,3 +1,14 @@
+require 'bike_container'
+
 class Garage
+
+include BikeContainer
+
+def accept(bikes)
+	bikes.each do |bike|
+	bike.fix!
+	dock bike
+	end
+end
 
 end
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -2,6 +2,20 @@ require 'garage'
 
 describe Garage do
 
+let(:garage) {Garage.new}
+let(:bike) {Bike.new}
+let(:van) {Van.new}
 
+it "should fix a docked bike" do
+	garage.accept [bike]
+	expect(bike).not_to be_broken
+end
+
+it "should fix multiple docked bikes" do
+	bikes = [bike, Bike.new, Bike.new]
+	garage.accept bikes
+	faulty_bikes = garage.bikes.select {|bike| bike.broken? }
+	expect(faulty_bikes).to be_empty
+end
 
 end
